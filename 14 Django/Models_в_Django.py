71f5b55a7db21b0b440aad2604b0ce64d66842c3
ecstasybtw <ecstasybtwru@gmail.com>
2025-12-29 14:26:48 +0500

    14 Django models.py change

--- 14 Django/Models_в_Django.py
+++ 14 Django/Models_в_Django.py
@@ -2,10 +2,28 @@ from django.db import models
 
 
 class SiteUser(models.Model):
-    # TODO: реализовать модель для взаимодействия с таблицей site_users
-    pass
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def get_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.get_name()
+
+    class Meta:
+        db_table = 'site_users'
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
 
 
 class Vacancy(models.Model):
-    # TODO: реализовать модель для взаимодействия с таблицей vacancies
-    pass
+    name = models.TextField()
+    salary = models.FloatField()
+    area_name = models.TextField()
+    published_at = models.TextField()
+
+    class Meta:
+        db_table = 'vacancies'
+        verbose_name = 'вакансия'
+        verbose_name_plural = 'вакансии'
